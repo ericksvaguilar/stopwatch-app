b975ea53cfd5c3171f951b7a5e2fbb00e28ef4cf
Erick Aguilar <ericksvaguilar@gmail.com>
2021-03-05 11:35:40 -0300

    feat(clock.jsx): create time state and add first html structure

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,5 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Clock = () => {
-  return <div></div>
+  const [time, setTime] = useState('00:00:00')
+
+  return (
+    <div>
+      <p>
+        <time dateTime={time}>{time}</time>
+      </p>
+    </div>
+  )
 }
