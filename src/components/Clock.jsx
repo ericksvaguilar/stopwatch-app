7487079d8c05f49eae3e6aeb3cdfc3eb28b8125d
Erick Aguilar <ericksvaguilar@gmail.com>
2021-03-05 12:42:55 -0300

    feat(clock.jsx): add running state and start-stop functions

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,12 +1,34 @@
 import React, { useState } from 'react'
 
 export const Clock = () => {
-  const [time, setTime] = useState('00:00:00')
+  const [time, setTime] = useState(0)
+  const [isRunning, setIsRunning] = useState(false)
+  const [intervalId, setIntervalId] = useState(0)
+
+  const startClock = () => {
+    setIsRunning(true)
+
+    const id = setInterval(() => {
+      setTime(prev => prev + 1)
+    }, 10)
+
+    setIntervalId(id)
+  }
+
+  const stopClock = () => {
+    setIsRunning(false)
+    clearInterval(intervalId)
+  }
 
   return (
     <div>
       <p>
         <time dateTime={time}>{time}</time>
+        {isRunning ? (
+          <button onClick={stopClock}>Stop</button>
+        ) : (
+          <button onClick={startClock}>Start</button>
+        )}
       </p>
     </div>
   )
