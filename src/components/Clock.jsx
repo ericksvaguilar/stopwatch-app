d329d044ebe1b14922ccd216cbd741cba788bce0
Erick Aguilar <ericksvaguilar@gmail.com>
2021-03-05 14:55:06 -0300

    feat(clock.jsx): change to use useEffect

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,35 +1,32 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Clock = () => {
   const [time, setTime] = useState(0)
   const [isRunning, setIsRunning] = useState(false)
-  const [intervalId, setIntervalId] = useState(0)
 
-  const startClock = () => {
-    setIsRunning(true)
+  useEffect(() => {
+    let intervalId = null
 
-    const id = setInterval(() => {
-      setTime(prev => prev + 1)
-    }, 10)
+    if (isRunning) {
+      intervalId = setInterval(() => setTime(prev => prev + 10), 10)
+    } else {
+      clearInterval(intervalId)
+    }
 
-    setIntervalId(id)
-  }
-
-  const stopClock = () => {
-    setIsRunning(false)
-    clearInterval(intervalId)
-  }
+    return () => clearInterval(intervalId)
+  }, [isRunning])
 
   return (
     <div>
       <p>
+        <time dateTime='00'>00</time>:<time dateTime='00'>00</time>:
         <time dateTime={time}>{time}</time>
-        {isRunning ? (
-          <button onClick={stopClock}>Stop</button>
-        ) : (
-          <button onClick={startClock}>Start</button>
-        )}
       </p>
+      {isRunning ? (
+        <button onClick={() => setIsRunning(false)}>Stop</button>
+      ) : (
+        <button onClick={() => setIsRunning(true)}>Start</button>
+      )}
     </div>
   )
 }
