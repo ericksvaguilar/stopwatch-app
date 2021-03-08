e2ccd344044e0bb9eca60ff13213c9e70b1e887c
Erick Aguilar <ericksvaguilar@gmail.com>
2021-03-08 10:49:24 -0300

    feat(app.jsx): change heading 1 text

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Clock } from './components/Clock'
 export const App = () => {
   return (
     <div>
-      <h1>Hello world</h1>
+      <h1>Stopwatch App</h1>
       <Clock />
     </div>
   )
