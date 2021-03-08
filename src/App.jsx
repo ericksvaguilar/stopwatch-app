883d6952a6ccc2a2b4966b6b1b074f1ac80dc7a7
Erick Aguilar <ericksvaguilar@gmail.com>
2021-03-08 10:58:55 -0300

    feat(app.jsx): import css module to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { Clock } from './components/Clock'
+import styles from './styles/App.module.css'
 
 export const App = () => {
   return (
-    <div>
+    <div className={styles.appContainer}>
       <h1>Stopwatch App</h1>
       <Clock />
     </div>
